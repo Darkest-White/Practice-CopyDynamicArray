42cc5b06a660e602d6b856d485986f07e57f008f
Darkest-White <darkest.white.fox@gmail.com>
2023-07-06 19:03:49 +0300

    Update

--- Practice-CopyDynamicArray/Main.cpp
+++ Practice-CopyDynamicArray/Main.cpp
@@ -2,19 +2,19 @@
 using namespace std;
 
 
-void FillArr(int* const arr, const int size)
+void fill_arr(int* const arr, const int SIZE)
 {
-	srand(time(0));
+	srand(SIZE);
 
-	for (int i = 0; i < size; i++)
+	for (int i = 0; i < SIZE; i++)
 	{
 		arr[i] = rand() % 11;
 	}
 }
 
-void PrintArr(int* const arr, const int size)
+void print_arr(const int* arr, const int SIZE)
 {
-	for (int i = 0; i < size; i++)
+	for (int i = 0; i < SIZE; i++)
 	{
 		cout << arr[i] << " ";
 	}
@@ -22,34 +22,37 @@ void PrintArr(int* const arr, const int size)
 }
 
 
+void copy_arr(int*& arr1, const int* arr2, const int size2)
+{
+	delete[] arr1;
+	arr1 = new int[size2];
+
+	for (int i = 0; i < size2; i++)
+	{
+		arr1[i] = arr2[i];
+	}
+}
+
+
 int main()
 {
 	int size1 = 5, size2 = 10;
 
-	int* p1 = new int[size1] {};
+	int* p1 = new int[size1];
+	fill_arr(p1, size1);
 	int* p2 = new int[size2];
-	FillArr(p2, size2);
+	fill_arr(p2, size2);
 
 	cout << "p1 = ";
-	PrintArr(p1, size1);
+	print_arr(p1, size1);
 	cout << "p2 = ";
-	PrintArr(p2, size2);
+	print_arr(p2, size2);
 
-	cout << "===============" << endl;
-
-	delete[] p1;
-	p1 = new int[size2];
-
-	for (int i = 0; i < size2; i++)
-	{
-		p1[i] = p2[i];
-	}
+	copy_arr(p1, p2, size2);
+	cout << "==============" << endl;
 
 	cout << "p1 = ";
-	PrintArr(p1, size2);
+	print_arr(p1, size2);
 	cout << "p2 = ";
-	PrintArr(p2, size2);
-
-	delete[] p1;
-	delete[] p2;
+	print_arr(p2, size2);
 }
